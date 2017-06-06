d1f2271cff07acf10bb7afe5a0dd9d90099b8600
Chaitanya Raman <chraman@users.noreply.github.com>
2017-06-06 18:21:27 +0530

    adding Tournaments stats

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="container">
+  <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1>Tournament Stats</h1>
+		<div>
+        <table class="table table-striped table-bordered">
+          <tr>
+            <th>Matches</th>
+            <th>Runs</th>
+            <th>Wickets</th>
+            <th>Sixes</th>
+          </tr>
+          <tr>
+            <td>577</td>
+            <td>1022012</td>
+            <td>10000</td>
+            <td>20000</td>
+          </tr>
+        </table>
+		</div>
+      </div>
+ </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
         <h1>Does winning toss wins you the match?</h1>
